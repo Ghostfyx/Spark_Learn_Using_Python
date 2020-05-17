efe00bcc72a88e9a9630ad5f6b3df3c4f616ab34
Ghostfyx <1078101544@qq.com>
2020-05-18 04:12:49 +0800

    Spark权威指南第四章代码

--- Spark_The_Definitive_Guide/chap04_Structured_APIs/Structured_APIs.py
+++ Spark_The_Definitive_Guide/chap04_Structured_APIs/Structured_APIs.py
@@ -10,6 +10,7 @@
 import os
 
 from pyspark.sql import SparkSession
+from pyspark.sql.types import *
 
 PYSPARK_PYTHON = "/Library/Frameworks/Python.framework/Versions/3.6/bin/python3"
 os.environ["PYSPARK_PYTHON"] = PYSPARK_PYTHON
@@ -31,4 +32,22 @@ column = spark.range(2).toDF("number")
 +------+
 '''
 column.show()
+
+# Scala与Python类型对照
+b = ByteType()
+print(b)
+i = IntegerType()
+a = ShortType()
+c = LongType()
+d = FloatType()
+e = DoubleType()  # Python float
+f = DecimalType()
+g = StringType()
+h = BinaryType()  # python bytearray
+j = TimestampType()
+k = DateType()
+l = ArrayType()
+m = MapType(StringType(), IntegerType())  # Python dict
+n = StructType()  # Python 元组或列表
+o = StructField("f1", StringType(), True)
 
